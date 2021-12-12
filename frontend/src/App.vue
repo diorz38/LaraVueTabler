9703c6e791b22d1e9d21bd182196e3c9728afa53
diorz38 <diorz38@yahoo.co.id>
2021-12-13 02:29:39 +0700

    add tabler to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue';
-import NavBar from './components/NavBar.vue';
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-    <div class="wrapper">
-      <Header />
-      <NavBar />
-      <HelloWorld msg="Hello Vue 3 + Vite + Tabler" />
-    </div>
+  <div class="wrapper">
+    <Header />
+    <NavBar />
+    <HelloWorld msg="Hello Vue 3 + Vite + Tabler" />
+  </div>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
 </template>
-
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
