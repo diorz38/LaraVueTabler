0f2eaa8b32f28e327bcf3f966d84526482a749ff
diorz38 <diorz38@users.noreply.github.com>
2022-06-23 02:26:52 +0700

    append avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Auth;
 use Inertia\Inertia;
-use Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class UsersController extends Controller
 {
